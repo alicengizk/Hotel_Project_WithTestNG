9acd75b5aa5f9c6f3e17de4aea7b6efe4b0afe7c
alicengizk <alicengizk@gmail.com>
2022-07-08 23:02:15 +0300

    All Us Are Done

--- src/test/java/Tests/Us10.java
+++ src/test/java/Tests/Us10.java
@@ -1,6 +1,7 @@
 package Tests;
 
 import Pages.CHPage;
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -14,13 +15,14 @@ import utilities.ReusableMethods;
 import java.util.ArrayList;
 
 public class Us10 {
-    CHPage chPage=new CHPage();
-    Actions actions=new Actions(Driver.getDriver());
+    CHPage chPage = new CHPage();
+    Actions actions = new Actions(Driver.getDriver());
+    Faker faker=new Faker();
 
     @Test
-    public void test01(){
+    public void test01() {
         chPage.anasayfa();
-        actions.sendKeys(Keys.PAGE_DOWN,Keys.PAGE_DOWN).perform();
+        actions.sendKeys(Keys.PAGE_DOWN, Keys.PAGE_DOWN,Keys.PAGE_DOWN,Keys.PAGE_DOWN).perform();
         Driver.getDriver().findElement(By.xpath("(//a[@href='/RoomDetail/676'])[2]")).click();
         Driver.getDriver().navigate().back();
         ReusableMethods.waitFor(1);
@@ -30,7 +32,7 @@ public class Us10 {
         Driver.getDriver().findElement(By.xpath("(//a[@href='/RoomDetail/674'])[2]")).click();
         Driver.getDriver().navigate().back();
         ReusableMethods.waitFor(1);
-        actions.sendKeys(Keys.ARROW_DOWN,Keys.ARROW_DOWN,Keys.ARROW_DOWN,Keys.ARROW_DOWN).perform();
+        actions.sendKeys(Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN,Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ARROW_DOWN).perform();
         Driver.getDriver().findElement(By.xpath("(//a[@href='/RoomDetail/673'])[2]")).click();
         Driver.getDriver().navigate().back();
         ReusableMethods.waitFor(1);
@@ -43,15 +45,16 @@ public class Us10 {
     }
 
     @Test
-    public void test02(){
+    public void test02() {
         chPage.anasayfa();
         chPage.roomsButon.click();
-        String roomTitle=Driver.getDriver().getTitle();
+        String roomTitle = Driver.getDriver().getTitle();
         Assert.assertTrue(roomTitle.contains("Rooms"));
+        Driver.closeDriver();
     }
 
     @Test
-    public void test03(){
+    public void test03() {
         test02();
         ReusableMethods.waitFor(1);
         Driver.getDriver().findElement(By.id("checkin_date")).clear();
@@ -59,18 +62,19 @@ public class Us10 {
         ReusableMethods.waitFor(1);
         Driver.getDriver().findElement(By.id("checkout_date")).clear();
         Driver.getDriver().findElement(By.id("checkout_date")).sendKeys("7/21/2022");
-        ReusableMethods.dropDownMenuId("IDRoomType",2);
-        ReusableMethods.dropDownMenuId("AdultCount",2);
-        ReusableMethods.dropDownMenuId("ChildrenCount",2);
+        ReusableMethods.dropDownMenuId("IDRoomType", 2);
+        ReusableMethods.dropDownMenuId("AdultCount", 2);
+        ReusableMethods.dropDownMenuId("ChildrenCount", 2);
         Driver.getDriver().findElement(By.id("location")).click();
         Driver.getDriver().findElement(By.xpath("//input[@type='submit']")).click();
-        String checkIndate=Driver.getDriver().findElement(By.id("checkin_date")).getText();
+        String checkIndate = Driver.getDriver().findElement(By.id("checkin_date")).getText();
         System.out.println(checkIndate);
         //Assert.assertTrue(Driver.getDriver().findElement(By.id("checkin_date")).getText().contains("7/15/2022"));
+        Driver.closeDriver();
     }
 
     @Test
-    public void test04(){
+    public void test04() {
         test03();
         Driver.getDriver().findElement(By.xpath("(//a[@href='/RoomDetail/2'])[3]")).click();
         Driver.getDriver().findElement(By.xpath("//a[@href='/Rooms/6']")).click();
@@ -98,34 +102,78 @@ public class Us10 {
         actions.sendKeys(Keys.PAGE_DOWN).perform();
         ReusableMethods.waitFor(1);
         Driver.getDriver().findElement(By.xpath("//a[@href='/Rooms/322']")).click();
+        Driver.closeDriver();
     }
 
     @Test
-    public void test05(){
+    public void test05() {
         chPage.anasayfa();
         chPage.logInButon.click();
         Driver.getDriver().findElement(By.xpath("//span[@class='btn btn-primary py-3 px-5']")).click();
         Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//h1[@class='mb-4 bread']")).getText().contains("Register"));
+        Driver.closeDriver();
     }
 
     @Test
-    public void test06(){
+    public void test06() {
         test05();
         Driver.getDriver().findElement(By.id("UserName")).sendKeys("alicengiz1");
         Driver.getDriver().findElement(By.id("Password")).sendKeys("Alicengiz1!");
-        Driver.getDriver().findElement(By.id("Email")).sendKeys("alicengiz@gmail.com");
+        Driver.getDriver().findElement(By.id("Email")).sendKeys("alicengizk@gmail.com");
         Driver.getDriver().findElement(By.id("NameSurname")).sendKeys("Ali Cengiz Kasar");
         Driver.getDriver().findElement(By.id("PhoneNo")).sendKeys("111 111-1111");
         Driver.getDriver().findElement(By.id("SSN")).sendKeys("111-11-1111");
         Driver.getDriver().findElement(By.id("DrivingLicense")).sendKeys("321456771");
-        ReusableMethods.dropDownMenuIdValue("IDCountry","243");
-       // ReusableMethods.dropDownMenuId("IDState",0);
+        ReusableMethods.dropDownMenuIdValue("IDCountry", "243");
+        // ReusableMethods.dropDownMenuId("IDState",0);
         Driver.getDriver().findElement(By.id("Address")).sendKeys("e gazi mah");
         Driver.getDriver().findElement(By.id("WorkingSector")).sendKeys("IT");
         Driver.getDriver().findElement(By.id("BirthDate")).sendKeys("6/3/1982");
         Driver.getDriver().findElement(By.id("btnSubmit")).click();
         Driver.getDriver().findElement(By.xpath("//button[@data-bb-handler='ok']")).click();
-        
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void test07() {
+        chPage.anasayfa();
+        chPage.logInButon.click();
+        Driver.getDriver().findElement(By.id("UserName")).sendKeys("alicengiz1");
+        Driver.getDriver().findElement(By.id("Password")).sendKeys("Alicengiz1!");
+        Driver.getDriver().findElement(By.id("btnSubmit")).click();
+        System.out.println(Driver.getDriver().findElement(By.xpath("(//div[@class='form-group'])[1]")).getText());
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//div[@class='form-group'])[1]")).getText().contains("alicengiz1"));
+        Driver.getDriver().findElement(By.id("navRooms")).click();
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        Driver.getDriver().findElement(By.xpath("(//a[@href='/RoomDetail/676'])[3]")).click();
+        Driver.getDriver().findElement(By.id("checkin_date")).clear();
+        Driver.getDriver().findElement(By.id("checkin_date")).sendKeys("7/15/2022");
+        Driver.getDriver().findElement(By.id("checkout_date")).clear();
+        Driver.getDriver().findElement(By.id("checkout_date")).sendKeys("7/21/2022");
+        ReusableMethods.dropDownMenuId("AdultCount",1);
+        ReusableMethods.dropDownMenuId("ChildrenCount",2);
+        Driver.getDriver().findElement(By.id("NameOnCreditCard")).sendKeys(faker.name().fullName());
+        Driver.getDriver().findElement(By.id("CreditCardNumber")).sendKeys(faker.business().creditCardNumber());
+        ReusableMethods.dropDownMenuId("CreditCardExpirationYear",3);
+        ReusableMethods.dropDownMenuId("CreditCardExpirationMonth",4);
+        Driver.getDriver().findElement(By.id("CVV")).sendKeys("123");
+        Driver.getDriver().findElement(By.id("Message")).sendKeys("oda kiralama");
+        Driver.getDriver().findElement(By.xpath("//input[@type='submit']")).click();
+        Driver.getDriver().findElement(By.xpath("//input[@type='submit']")).click();
+        Driver.closeDriver();
+    }
+    @Test
+    public void test08(){
+        test07();
+        Driver.getDriver().findElement(By.xpath("(//a[@href='/Profile/ReservationDetail/1908'])[2]")).clear();
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void test09(){
+        test08();
+        Driver.getDriver().findElement(By.xpath("//button[@data-bb-handler='ok']")).click();
+        Driver.closeDriver();
     }
 
 }
